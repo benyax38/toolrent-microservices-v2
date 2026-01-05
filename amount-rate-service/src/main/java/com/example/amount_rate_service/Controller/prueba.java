d600d1ae138e2f64a82bea22e78253b7bcadd56f
benyax38 <141201925+benyax38@users.noreply.github.com>
2026-01-05 15:24:17 -0300

    amount-rate-service
    primer microservicio

---
+++ amount-rate-service/src/main/java/com/example/amount_rate_service/Controller/prueba.java
@@ -0,0 +1,15 @@
+package com.example.amount_rate_service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class prueba {
+
+    @GetMapping
+    public String health() {
+        return "amount-rate-service OK";
+    }
+}
